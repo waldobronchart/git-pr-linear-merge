727289d1c692c73cd4f2d1a352da1718dd8478a0
Waldo Bronchart <wbronchart@gmail.com>
2022-06-12 19:59:26 -0400

    Fixes issue where running locally actually runs installed version
    This is a dev only issue. Has no impact on script behavior

--- git-pr.py
+++ git-pr.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 DIR_PATH = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(DIR_PATH, 'src'))
+sys.path.insert(0, os.path.join(DIR_PATH, 'src'))
 
 from git_pr_linear_merge.main import run
 if __name__ == '__main__':
